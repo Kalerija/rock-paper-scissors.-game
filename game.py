7079ff41156f7fcafd0a3f614e6ca85693e13278
Kalerija <114050981+Kalerija@users.noreply.github.com>
2022-09-21 09:16:04 +0300

    Create game.py
    my first commit

---
+++ game.py
@@ -0,0 +1,13 @@
+human_turn = 'rock'
+computer_turn = 'scissors'
+
+if human_turn == computer_turn :
+   print('Draw!')
+elf human_turn == 'rock' and computer_turn 'scissors':
+   print('Human wins!')
+elf human_turn == 'paper' and computer_turn 'rock' :
+   print('Human wins!')
+elf human_turn == 'scissors' and computer_turn 'scissors' :
+    print('Human wins!')
+else:
+   print('Computer wins!')
