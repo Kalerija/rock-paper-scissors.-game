e3ea138f80787e35a96742193d3b9e42570b2618
Kalerija <114050981+Kalerija@users.noreply.github.com>
2022-09-21 09:34:33 +0300

    Update game.py

--- game.py
+++ game.py
@@ -3,11 +3,11 @@ computer_turn = 'scissors'
 
 if human_turn == computer_turn:
    print('Draw!')
-elif human_turn == 'rock' and computer_turn 'scissors':
+elif human_turn == 'rock' and computer_turn == 'scissors':
    print('Human wins!')
-elif human_turn == 'paper' and computer_turn 'rock' :
+elif human_turn == 'paper' and computer_turn =='rock':
    print('Human wins!')
-elif human_turn == 'scissors' and computer_turn 'scissors' :
+elif human_turn == 'scissors' and computer_turn == 'scissors':
    print('Human wins!')
 else:
    print('Computer wins!')
